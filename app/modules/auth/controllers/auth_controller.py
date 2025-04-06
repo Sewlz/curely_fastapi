87bb3bfa3867d65e9723c778a4106ee1ba4215ed
Hoaihop <122537273+Hoaihop@users.noreply.github.com>
2025-04-06 21:42:17 +0700

    fix

--- app/modules/auth/controllers/auth_controller.py
+++ app/modules/auth/controllers/auth_controller.py
@@ -90,68 +90,6 @@ def login_google(payload: GoogleLoginSchema, auth_service: AuthService = Depends
 
 
 
-
 @router.post("/forgot-password")
 def forgot_password(payload: ForgotPasswordSchema, auth_service: AuthService = Depends()):
     return auth_service.forgot_password(payload.email)
-
-# @router.post("/login-google", tags=["Auth"])
-# def login_google(payload: GoogleLoginSchema, auth_service: AuthService = Depends()):
-#     """
-#     API để đăng nhập bằng Google.
-#     """
-#     # In ra log kiểm tra payload
-#     print(f"Received payload: {payload}")
-
-#     # Gửi id_token về Supabase để xác thực
-#     result = auth_service.login_with_google(payload.id_token)
-    
-#     # Kiểm tra kết quả trả về từ auth_service
-#     if not result:
-#         raise HTTPException(status_code=400, detail="Google login failed")
-    
-#     # Trả về kết quả khi đăng nhập thành công
-#     return result
-
-# @router.post("/login-facebook", tags=["Auth"])
-# def login_facebook(payload: FacebookLoginSchema, auth_service: AuthService = Depends()):
-#     """
-#     API để đăng nhập bằng Facebook.
-#     """
-#     print(f"Received payload: {payload}")  # ✅ Log kiểm tra payload
-#     result = auth_service.login_with_facebook(payload.id_token)
-#     if not result:
-#         raise HTTPException(status_code=400, detail="Facebook login failed")
-#     return result
-
-# @router.post("/refresh-token", tags=["Auth"])
-# def refresh_token(refresh_token: str, auth_service: AuthService = Depends()):
-#     result = auth_service.refresh_token(refresh_token)
-#     if not result:
-#         raise HTTPException(status_code=400, detail="Invalid refresh token")
-#     return {
-#         "message": "Token refreshed successfully",
-#         "idToken": result["idToken"],
-#         "refreshToken": result["refreshToken"],
-#         "expiresIn": result["expiresIn"]
-#     }
-
-
-
-
-
-# @router.get("/auth-user", tags=["Auth"])
-# def get_user_info(user: dict = Depends(auth_guard)):
-#     """
-#     Lấy thông tin tài khoản từ Firestore.
-#     """
-#     user_id = user["uid"]
-#     user_data = AuthRepository.get_user(user_id)
-
-#     if not user_data:
-#         raise HTTPException(status_code=404, detail="User not found")
-
-#     return {
-#         "message": "User retrieved successfully",
-#         "user": user_data
-#     }
