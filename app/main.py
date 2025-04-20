5cc98c46f75084a6647b54b2f33233bead76ae57
Sewlz <tnam7232@gmail.com>
2025-04-21 00:00:25 +0700

    remove cors in main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.modules.llm.controllers.llm_controller import router as llm_router
 from app.modules.cnn.controllers.cnn_controller import router as cnn_router
 from app.modules.user.controllers.user_controller import router as user_router
@@ -7,17 +6,9 @@ from app.modules.auth.controllers.auth_controller import router as auth_router
 from app.modules.admin.controllers.admin_controller import router as admin_router
 
 app = FastAPI(
-    title="Curely FastAPI",
+    title="Curely API",
     description="Curely Backend Application Using FastAPI",
     version="1.0.0",
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 app.include_router(admin_router, prefix="/admins", tags=["Admin"])
@@ -28,4 +19,4 @@ app.include_router(llm_router, prefix="/llm", tags=["LLM"])
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Curely API"}
+    return {"message": "Welcome to Curely API"}
